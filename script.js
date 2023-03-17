c4900a5c96c66d7abff3af3befc0154992c70d00
AbduTech <abdulqudoospk737@gmail.com>
2023-03-17 22:20:15 +0500

    added interest into javaScript file and add some changes in html file

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var rat = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var RateInPercent = rat / 100
+
+    var A = p * (1 + RateInPercent*years)
+
+    document.getElementById("result").innerHTML = A;
     
 }
         
+
